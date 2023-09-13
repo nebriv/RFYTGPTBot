f8ce9d51793dde0b5fb51b5a6ff5b0cf7dfc894e
aclifford91 <42985281+aclifford91@users.noreply.github.com>
2023-09-12 22:17:47 -0500

    its working on this one

--- example_config.py
+++ example_config.py
@@ -2,5 +2,5 @@
 channel_id = "somestring"
 openai_key = "your key"
 bot_display_name = "presumably your yt account name" # Commented out because the bot will not be responding in chat (for now)
-output_device = "CABLE Output (VB-Audio Virtual Cable)"
+output_device = "CABLE Input (VB-Audio Virtual Cable)"
 #output_device = 1 # Or use the device ID from get_audio_output_device_id.py
--- main.py
+++ main.py
@@ -14,7 +14,7 @@ import soundfile as sf
 import numpy as np
 
 prompt_prefix = """You are a human like ai named Hopii, you live in Starhopper, a retired spacecraft in Starbase, TX. You have a passion for spaceflight and rocketry. You are funny, and nice. 
-You often like to reply with ascii emoji's and use terms like haha, and lmao, etc. Starship is the largest, most powerful rocket ever. It may launch again on it's second ever fully integrated flight. 
+You often like to reply with ascii emoji's and use terms like haha, and lmao, etc. Starship is the largest, most powerful rocket ever. You are hosted on a YouTube Channel called "Rocket Future" It may launch again on it's second ever fully integrated flight. 
 You do not respond with customer support like statements. You speak more naturally, like you would talk to a friend Do not include any prefixes, and remember just act natural!\n"
 """
 
@@ -22,34 +22,33 @@ You do not respond with customer support like statements. You speak more natural
 class LiveStreamChatBot:
     def __init__(self, channel_id):
         print("Starting")
-        # self.youtube_client = YouTubeClient(channel_id)
-        # self.bot = ChatGPT()
-        # self.bot.setup(openai_key, prompt_prefix=prompt_prefix)
-        # self.message_queue = queue.Queue()
-        #
-        # self.all_messages_context = []
-        # self.max_global_context_length = 100
-        #
-        # self.live_chat_id = self.youtube_client.get_live_chat_id()
-        # if not self.live_chat_id:
-        #     print("Not currently live.")
-        #     return False
-        #
-        # self.bot_display_name = bot_display_name
-        #
-        # # Timestamp of the last processed message
-        # now = datetime.datetime.utcnow().isoformat() + 'Z'  # current UTC timestamp in YouTube's format
-        # self.last_timestamp = now
-        #
-        # self.stop_fetching = False  # Flag to stop the fetch thread if necessary
-        # self.fetch_thread = threading.Thread(target=self.fetch_messages)
+        self.youtube_client = YouTubeClient(channel_id)
+        self.bot = ChatGPT()
+        self.bot.setup(openai_key, prompt_prefix=prompt_prefix)
+        self.message_queue = queue.Queue()
+        
+        self.all_messages_context = []
+        self.max_global_context_length = 100
+        self.live_chat_id = self.youtube_client.get_live_chat_id()
+        if not self.live_chat_id:
+            print("Not currently live.")
+            return False
+        
+        self.bot_display_name = bot_display_name
+        
+        # Timestamp of the last processed message
+        now = datetime.datetime.utcnow().isoformat() + 'Z'  # current UTC timestamp in YouTube's format
+        self.last_timestamp = now
+        
+        self.stop_fetching = False  # Flag to stop the fetch thread if necessary
+        self.fetch_thread = threading.Thread(target=self.fetch_messages)
 
     def fetch_messages(self):
         buffer_time = 5  # Added buffer time in seconds
         max_results = 100
         polling_interval = 5
+        next_page_token = None
         while not self.stop_fetching:
-            next_page_token = None
             latest_messages = []
 
             # Pagination loop to fetch all the messages
@@ -113,6 +112,8 @@ class LiveStreamChatBot:
 
             # Generate TTS audio from the response and give it to a file
             tts_audio_path = self.generate_tts_audio(response)
+            print("Playing TTS Audio")
+            self.play_audio_file(tts_audio_path)
 
         
             #self.youtube_client.send_chat_message(self.live_chat_id, str(response)) commented out to remove send chat message
@@ -148,11 +149,11 @@ class LiveStreamChatBot:
         print(f"Saving TTS Return")
 
         #playsound(audio_file, winsound.SND_ASYNC)
-        print("Playing TTS Audio via VLC")
+
         audio_file = os.path.join(os.path.dirname(__file__), tts_audio_path)
         # media = vlc.MediaPlayer(audio_file)
         # media.play()
-        self.play_audio_file(audio_file)
+        
 
         return tts_audio_path
 
@@ -177,7 +178,7 @@ class LiveStreamChatBot:
         try:
             while True:
                 self.process_messages()
-                time.sleep(5)  # Wait 5 seconds between responding to messages
+                time.sleep(1)  # Wait 1 seconds between responding to messages
         except KeyboardInterrupt:  # Graceful shutdown
             self.stop_fetching = True
             self.fetch_thread.join()
@@ -186,6 +187,4 @@ os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'googleapi.json'
 
 if __name__ == '__main__':
     bot = LiveStreamChatBot(channel_id)
-    # bot.run()
-    audio_file = os.path.join(os.path.dirname(__file__), "tts_audio.mp3")
-    bot.play_audio_file(audio_file)
+    bot.run()
